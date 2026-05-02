811d2b2e7db0316b66babf4446df5241dd313b03
Searchstars <searchstars@163.com>
2026-05-03 00:46:08 +0800

    Revert "更新依赖"
    This reverts commit e860135534f638a811cffcb9a01f747ea09531eb.

--- src/stdimp.rs
+++ src/stdimp.rs
@@ -1022,9 +1022,7 @@ impl BluetoothInterface for StdImp {
             }
 
             #[cfg(not(target_os = "android"))]
-            ConnectType::BLE => {
-                tauri::async_runtime::block_on(self.ble_send_impl(data, characteristic))
-            }
+            ConnectType::BLE => tauri::async_runtime::block_on(self.ble_send_impl(data, characteristic)),
 
             #[cfg(target_os = "android")]
             ConnectType::BLE => Err(SendError::Disconnected),
